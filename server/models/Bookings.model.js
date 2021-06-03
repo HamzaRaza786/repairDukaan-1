cef3c055adae7084e844a7a04f3c01a7bc9cd5e6
Syed Muhammad Ali <m.ali.2000@live.com>
2021-06-04 00:51:56 +0500

    Update and rename Bookings.js to Bookings.model.js

---
+++ server/models/Bookings.model.js
@@ -0,0 +1,18 @@
+//workers.model.js
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const bookingsSchema = new Schema({
+  ID: { type: String, required: true },
+  Type: { type: String, required: true },
+  date: { type: Date, required: true },
+  Location: { type: String, required: true },
+  Fare: { type: String, required: true },
+  Status: { type: Number, required: true },
+  Email: { type: String, required: true }
+});
+
+const Bookings = mongoose.model('Bookings', bookingsSchema);
+
+module.exports = Bookings;
